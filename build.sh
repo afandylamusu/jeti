bb942960aa97a2b0865268604bf13ff1f53cf104
afandy.lamusu <afandy.lamusu@live.co.uk>
2017-10-05 13:59:11 +0700

    update travis

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sudo docker-compose -f docker-compose.yml -f docker-compose.override.yml up
-sudo dotnet test ./Jet.Service.Manifest.UnitTest.Client/Jet.Service.Manifest.UnitTest.Client.csproj
+docker-compose -f docker-compose.yml -f docker-compose.override.yml up
+dotnet test ./Jet.Service.Manifest.UnitTest.Client/Jet.Service.Manifest.UnitTest.Client.csproj
